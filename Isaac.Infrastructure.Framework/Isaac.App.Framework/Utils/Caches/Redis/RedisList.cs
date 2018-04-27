88331051937e93098f28e33c8defa9e16ce873a6
Shiwen Shen <vikirobin@outlook.com>
2018-04-27 11:24:27 +0800

    实现了Redis LIst操作

--- Isaac.Infrastructure.Framework/Isaac.App.Framework/Utils/Caches/Redis/RedisList.cs
+++ Isaac.Infrastructure.Framework/Isaac.App.Framework/Utils/Caches/Redis/RedisList.cs
@@ -1,4 +1,5 @@
 ﻿using Isaac.Infrastructure.Framework.Utils;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,109 @@ namespace Isaac.App.Framework.Utils.Caches.Redis
 {
     public class RedisList : RedisBase, IRdsList
     {
+        public string Dequeue(string key)
+        {
+            var value = Core.ListLeftPop(key);
+            if (value.HasValue)
+            {
+                return value.ToString();
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public T Dequeue<T>(string key)
+        {
+            var value = this.Dequeue(key);
+            if (!string.IsNullOrEmpty(value))
+            {
+                return JsonConvert.DeserializeObject<T>(value.ToString());
+            }
+            else
+            {
+                return default(T);
+            }
+        }
+
+        public string DequeueInverse(string key)
+        {
+            var value = Core.ListRightPop(key);
+            if (value.HasValue)
+            {
+                return value.ToString();
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public T DequeueInverse<T>(string key)
+        {
+            var value = this.DequeueInverse(key);
+            if (!string.IsNullOrEmpty(value))
+            {
+                return JsonConvert.DeserializeObject<T>(value.ToString());
+            }
+            else
+            {
+                return default(T);
+            }
+        }
+
+        public void Enqueue(string key, object value)
+        {
+            Core.ListRightPush(key, JsonConvert.SerializeObject(value));
+        }
+
+        public void Enqueue(string key, object value, TimeSpan expireSpan)
+        {
+            this.Enqueue(key, value);
+            this.SetExpireSpan(key, expireSpan);
+        }
+
+        public void EnqueueInverse(string key, object value)
+        {
+            Core.ListLeftPush(key, JsonConvert.SerializeObject(value));
+        }
+
+        public void EnqueueInverse(string key, object value, TimeSpan expireSpan)
+        {
+            this.EnqueueInverse(key, value);
+            this.SetExpireSpan(key, expireSpan);
+        }
+
+        public long Length(string key)
+        {
+            return Core.ListLength(key);
+        }
+
+        public string Get(string key, long index)
+        {
+            var result = Core.ListGetByIndex(key, index);
+            if (result.HasValue)
+            {
+                return result.ToString();
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public T Get<T>(string key, int index)
+        {
+            var result = Get(key, index);
+            if (string.IsNullOrEmpty(result))
+            {
+                return JsonConvert.DeserializeObject<T>(result);
+            }
+            else
+            {
+                return default(T);
+            }
+        }
     }
 }
--- Isaac.Infrastructure.Framework/Isaac.Infrastructure.Interfaces/Utils/Cache/Redis/IRdsList.cs
+++ Isaac.Infrastructure.Framework/Isaac.Infrastructure.Interfaces/Utils/Cache/Redis/IRdsList.cs
@@ -8,5 +8,20 @@ namespace Isaac.Infrastructure.Framework.Utils
 {
     public interface IRdsList
     {
+        void Enqueue(string key, object value);
+
+        void Enqueue(string key, object value, TimeSpan expireSpan);
+
+        void EnqueueInverse(string key, object value);
+
+        void EnqueueInverse(string key, object value, TimeSpan expireSpan);
+
+        string Dequeue(string key);
+
+        string DequeueInverse(string key);
+
+        long Length(string key);
+
+        string Get(string key, long index);
     }
 }
