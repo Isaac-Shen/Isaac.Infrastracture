fe00cd1a62d853f2613674ab84f49fdea77bc1fc
Shiwen Shen <vikirobin@outlook.com>
2018-04-28 16:57:11 +0800

    完成对Mvc5的支持

---
+++ Isaac.Infrastructure.Framework/Isaac.Infrastructure.Common/Extension/ExceptionExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Isaac.Infrastructure.Common.Extension
+{
+    public static class ExceptionExtension
+    {
+        public static string AllMessage(this Exception exception)
+        {
+            if (exception.InnerException != null)
+            {
+                return string.Format("[{2}]{0}\n-->{1}", 
+                    exception.Message, 
+                    exception.InnerException.AllMessage(),
+                    exception.GetType().Name);
+            }
+            else
+            {
+                return exception.Message;
+            }
+        }
+    }
+}
