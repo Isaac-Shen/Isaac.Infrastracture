18b055ee4a425df1ed3fc22445318e32f804c9ce
Shiwen Shen <vikirobin@outlook.com>
2018-05-08 16:36:49 +0800

    登陆页面准备

---
+++ Isaac.Infrastructure.Framework/Isaac.SampleMvc/Scripts/common/login/login.js
@@ -0,0 +1,15 @@
+﻿$('#button-login').on('click', function () {
+    var name = $('#inputUserId').val();
+    var pwd = $('#inputPassword').val();
+
+    //检测网络状态
+    if (!window.navigator.onLine) {
+        return;
+    }
+
+    $.post('/Login/Login', { userId: name, password: pwd, type: 0 }, function (loginResult) {
+        if (d.success) {
+            location.href = loginResult.result.url;
+        }
+    });
+});
