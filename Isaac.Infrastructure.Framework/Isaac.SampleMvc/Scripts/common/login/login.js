124e1c9cf94e106ee5345244c37dae21898426c4
Shiwen Shen <vikirobin@outlook.com>
2018-05-10 12:03:02 +0800

    修复了一个登陆反应失效的问题

--- Isaac.Infrastructure.Framework/Isaac.SampleMvc/Scripts/common/login/login.js
+++ Isaac.Infrastructure.Framework/Isaac.SampleMvc/Scripts/common/login/login.js
@@ -7,19 +7,19 @@
         return;
     }
 
-    //if (name.length * pwd.length !== 0) {
-        $.post('/Login/Login', { userId: name, password: pwd, type: 0 }, function (loginResult) {
-            if (loginResult.success) {
-                location.href = loginResult.result.url;
-            }
-            else {
-                showErrorMessage(loginResult.result + ": 不合法的登陆!请检查用户名密码!");
-            }
-        });
-    //}
-    //else{
-    //    showErrorMessage("无法在用户名密码缺失的情况下登陆!请填写用户名密码!");
-    //}
+    if (name.length * pwd.length === 0) {
+        showErrorMessage("无法在用户名密码缺失的情况下登陆!请填写用户名密码!");
+        return;
+    }
+
+    $.post('/Login/Login', { userId: name, password: pwd, type: 0 }, function (loginResult) {
+        if (loginResult.success) {
+            location.href = loginResult.result.url;
+        }
+        else {
+            showErrorMessage(loginResult.result + ": 不合法的登陆!请检查用户名密码!");
+        }
+    });
 });
 
 function showErrorMessage(message) {
